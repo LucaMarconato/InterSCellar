b0c8c60a7b214c2aa99c8196fcf03086969f90cc
Eunice Lee <eunicelee153@gmail.com>
2025-11-12 17:49:45 -0500

    Added deployment for documentation

---
+++ docs/conf.py
@@ -0,0 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
+import interscellar
+
+project = 'InterSCellar'
+copyright = '2025, HIDIVE Lab'
+author = 'Eunice Lee'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.autosummary", "sphinx.ext.napoleon", "sphinx.ext.viewcode", "myst_parser"]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'English'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_book_theme'
+html_static_path = ['_static']
+
+# GitHub Pages URL - Update this when the repository is transferred
+# Format: https://<org-or-username>.github.io/<repo-name>/
+html_baseurl = os.environ.get('SPHINX_HTML_BASEURL', 'https://eunicelee153.github.io/InterSCellar/')
---
+++ docs/index.rst
@@ -0,0 +1,17 @@
+.. InterSCellar documentation master file, created by
+   sphinx-quickstart on Wed Nov 12 17:34:42 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+InterSCellar documentation
+==========================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
